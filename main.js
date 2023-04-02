75cb62cd5da80fe7587483e9e0fd45681be3e62a
Roman Brunka <rbrunka@gmail.com>
2023-04-03 00:18:29 +0200

    v.0.0.13

--- main.js
+++ main.js
@@ -15,9 +15,6 @@ const { url } = require('inspector');
 
 class DefroEmodul extends utils.Adapter {
 
-    /**
-     * @param {Partial<utils.AdapterOptions>} [options={}]
-     */
     constructor(options) {
         super({
             ...options,
@@ -28,64 +25,53 @@ class DefroEmodul extends utils.Adapter {
         this.refreshStateTimeout = null;
 
         this.on('ready', this.onReady.bind(this));
-        //this.on('stateChange', this.onStateChange.bind(this));
-        // this.on('objectChange', this.onObjectChange.bind(this));
-        // this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
     }
 
-    /**
-     * Is called when databases are connected and adapter received configuration.
-     */
     async onReady() {
-        // Initialize your adapter here
+        try {
+            if (!this.config.userId) {
+                this.log.error(`User ID is empty - please check instance configuration of ${this.namespace}`);
+                return;
+            }
+            if (!this.config.token) {
+                this.log.error(`Token is empty - please check instance configuration of ${this.namespace}`);
+                return;
+            }
+            if (!this.config.boilerUdid) {
+                this.log.error(`Boiler UDID is empty - please check instance configuration of ${this.namespace}`);
+                return;
+            }
 
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        if (!this.config.userId) {
-            this.log.error(`User ID is empty - please check instance configuration of ${this.namespace}`);
-            return;
+            this.log.debug('config userID: ' + this.config.userId);
+            this.log.debug('config token: ' + this.config.token);
+            this.log.debug('config boiler UDID: ' + this.config.boilerUdid);
+
+            // read config
+            const defroUserID = this.config.userId;
+            const defroToken = this.config.token;
+            const defroUDID = this.config.boilerUdid;
+
+            this.emodulApiClient = axios.create({
+                method: 'get',
+                baseURL: 'https://emodul.eu/api/v1/users/',
+                headers: { Authorization: 'Bearer ' + defroToken },
+                responseType: 'json',
+                responseEncoding: 'utf8',
+                timeout: 1000,
+                validateStatus: (status) => {
+                    return [200, 201, 401].includes(status);
+                },
+            });
+
+            const defroUrl = defroUserID + '/modules/' + defroUDID;
+            await this.refreshState(defroUrl);
+        } finally {
+            this.stop();
         }
-        if (!this.config.token) {
-            this.log.error(`Token is empty - please check instance configuration of ${this.namespace}`);
-            return;
-        }
-        if (!this.config.boilerUdid) {
-            this.log.error(`Boiler UDID is empty - please check instance configuration of ${this.namespace}`);
-            return;
-        }
-
-        await this.setApiConnection(false);
-
-        this.log.debug('config userID: ' + this.config.userId);
-        this.log.debug('config token: ' + this.config.token);
-        this.log.debug('config boiler UDID: ' + this.config.boilerUdid);
-
-        // read config
-        const defroUserID = this.config.userId;
-        const defroToken = this.config.token;
-        const defroUDID = this.config.boilerUdid;
-
-        this.emodulApiClient = axios.create({
-            method: 'get',
-            baseURL: 'https://emodul.eu/api/v1/users/',
-            headers: { Authorization: 'Bearer ' + defroToken },
-            responseType: 'json',
-            responseEncoding: 'utf8',
-            timeout: 1000,
-            validateStatus: (status) => {
-                return [200, 201, 401].includes(status);
-            },
-        });
-
-        const defroUrl = defroUserID + '/modules/' + defroUDID;
-        await this.refreshState(defroUrl);
-        await this.subscribeStatesAsync('*');
     }
 
     async refreshState(defroUrl) {
-        let nextRefreshSec = 60;
-
         try {
             const deviceInfoResponse = await this.emodulApiClient.get(defroUrl);;
             this.log.debug(`deviceInfoResponse ${deviceInfoResponse.status}: ${JSON.stringify(deviceInfoResponse.data)}`);
@@ -93,35 +79,18 @@ class DefroEmodul extends utils.Adapter {
             if (deviceInfoResponse.status == 200) {
                 const deviceData = deviceInfoResponse.data;
                 //console.log(deviceData);
-                await this.setApiConnection(true);
                 this.log.debug(`deviceData: ${JSON.stringify(deviceData)}`);
                 this.setState('JSON', {val: JSON.stringify(deviceData)}, true);
             }
         } catch (error) {
             // Set device offline
-            await this.setApiConnection(false);
-
             if (error.name === 'AxiosError') {
                 this.log.error(`Request to ${error.config.url} failed with code ${error.status} (${error.code}): ${error.message}`);
                 this.log.debug(`Complete error object: ${JSON.stringify(err)}`);
             } else {
                 this.log.error(error);
-            }
-        } finally {
-            if (this.refreshStateTimeout) {
-                this.clearTimeout(this.refreshStateTimeout);
-            }
-
-            this.refreshStateTimeout = this.setTimeout(() => {
-                this.refreshStateTimeout = null;
-                this.refreshState(defroUrl);
-            }, nextRefreshSec * 1000);
-            this.log.debug(`refreshStateTimeout: re-created refresh timeout: id ${this.refreshStateTimeout}`);
+            } 
         }
-    }
-
-    async setApiConnection(status) {
-        await this.setStateAsync('info.connection', { val: status, ack: true });
     }
 
 /*         // create JSON data point
@@ -220,57 +189,6 @@ class DefroEmodul extends utils.Adapter {
             callback();
         }
     }
-
-    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-    // /**
-    //  * Is called if a subscribed object changes
-    //  * @param {string} id
-    //  * @param {ioBroker.Object | null | undefined} obj
-    //  */
-    // onObjectChange(id, obj) {
-    //     if (obj) {
-    //         // The object was changed
-    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-    //     } else {
-    //         // The object was deleted
-    //         this.log.info(`object ${id} deleted`);
-    //     }
-    // }
-
-    /**
-     * Is called if a subscribed state changes
-     * @param {string} id
-     * @param {ioBroker.State | null | undefined} state
-     */
-    onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted`);
-        }
-    }
-
-    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-    //  * @param {ioBroker.Message} obj
-    //  */
-    // onMessage(obj) {
-    //     if (typeof obj === 'object' && obj.message) {
-    //         if (obj.command === 'send') {
-    //             // e.g. send email or pushover or whatever
-    //             this.log.info('send command');
-
-    //             // Send response in callback if required
-    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-    //         }
-    //     }
-    // }
-
 }
 
 if (require.main !== module) {
